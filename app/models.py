b90a13a5eca602844673c55f1354986844bbd3b4
magechaP <magechapeter@gmail.com>
2019-08-02 08:10:28 +0300

    create the various classes that will instantiate our news data

--- app/models.py
+++ app/models.py
@@ -2,39 +2,43 @@ class News:
     '''
     News class to define a news objects
     '''
-    def __init__(self,author,title,description,urlToImage,publishedAt,content):
-    
-        self.author=author
-        self.title=title
-        self.description=description
-        self.urlToImage= urlToImage
-        self.publishedAt=publishedAt
-        self.content=content
+
+    def __init__(self, author, title, description, urlToImage, publishedAt, content):
+
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+
 
 class Sources:
     '''
     Source class to define sources
     '''
-    def __init__(self,id,name,description,url,category,language,country):
-        self.id=id
-        self.name=name
-        self.description=description
-        self.url=url
-        self.category=category
-        self.language=language
-        self.country=country
+
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
 
 class Articles:
     '''
     article class to define an article
     '''
-    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
-    
-        self.author=author
-        self.title=title
-        self.description=description
-        self.url=url
-        self.urlToImage=urlToImage
-        self.publishedAt=publishedAt
-        self.content=content
 
+    def __init__(self, author, title, description, url, urlToImage, publishedAt, content):
+
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
