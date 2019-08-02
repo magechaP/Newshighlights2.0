16d2fdb50d0dd8369465e0dabf860dad0ba918fa
magechaP <magechapeter@gmail.com>
2019-08-02 08:05:52 +0300

    create test for checking if the articles are being created

--- tests/test_article.py
+++ tests/test_article.py
@@ -4,7 +4,7 @@ class ArticleTest(unittest.TestCase):
 
     def test_article(self):
         '''
-        test_source checks that new news source objects are created
+        test_source checks that new news source objects are created and instantiated
         '''
         self.new_article = Articles('me','bbc-news','Jesus','is alive','eng','kenya','no','who')
         self.assertTrue(isinstance(self.new_article,Articles))
