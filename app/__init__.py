f21f297c37147c3b773035eaab680cd292476f8b
magechaP <magechapeter@gmail.com>
2019-08-02 08:11:12 +0300

    initialize the app and fix configurations settings

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 
+
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -15,12 +16,11 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
-     # Registering the blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     # setting config
     from .request import configure_request
     configure_request(app)
-    
-    return app
 
+    return app
