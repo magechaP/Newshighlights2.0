465ca9347c52ca3bff6adc0fa62292830da2ea11
magechaP <magechapeter@gmail.com>
2019-08-02 08:08:21 +0300

    create test for checking behaviour of the sources class

--- tests/test_source.py
+++ tests/test_source.py
@@ -5,7 +5,7 @@ class SourceTest(unittest.TestCase):
 
     def test_source(self):
         '''
-        test_source checks that new news source objects are created
+        test_source checks that new news source objects are created and instantiated
         '''
         self.new_source = Sources(1,'bbc-news','Jesus','is alive','eng','who','kenya')
         self.assertTrue(isinstance(self.new_source,Sources))
