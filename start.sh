4beaf2e9088009b0b19b2cc6c618fda1adb408de
magechaP <magechapeter@gmail.com>
2019-08-02 09:52:30 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='42621ea7be204cc899887a2fc2856ab7'
-export SECRET_KEY='be@t$bydre'
+export NEWS_API_KEY='a735c785a71041dd8e302c9b7f607868'
+export SECRET_KEY='lanti'
 
 python3.6 manage.py server
